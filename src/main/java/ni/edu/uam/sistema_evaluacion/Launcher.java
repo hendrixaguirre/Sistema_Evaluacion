d5dab5029851528f9db6ee63cd3016b84e0df811
HDAZ <hendrixag@uamv.edu.ni>
2026-09-22 18:09:56 -0600

    Commit con la clase InicioSesion

---
+++ src/main/java/ni/edu/uam/sistema_evaluacion/Launcher.java
@@ -0,0 +1,9 @@
+package ni.edu.uam.sistema_evaluacion;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(InicioSesionApplication.class, args);
+    }
+}
---
+++ src/main/java/ni/edu/uam/sistema_evaluacion/controller/InicioSesionController.java
@@ -0,0 +1,4 @@
+package ni.edu.uam.sistema_evaluacion.controller;
+
+public class InicioSesionController {
+}
